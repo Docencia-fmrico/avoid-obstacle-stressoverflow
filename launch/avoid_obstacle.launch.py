34d0529440bed6db85b9a1478db4c10a222fb8f1
Teresa <mt.ortega.2021@alumnos.urjc.es>
2023-02-15 14:48:22 +0100

    Robot forward & turn

---
+++ launch/avoid_obstacle.launch.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Intelligent Robotics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    bumpgo_cmd = Node(package='avoid_obstacles_cpp',
+                      executable='avoidobstacles',
+                      output='screen',
+                      parameters=[{
+                        'use_sim_time': True
+                      }],
+                      remappings=[
+                        ('input_scan', '/scan_filtered'),
+                        ('output_vel', '/cmd_vel'),
+                        ('input_button', '/events/button')
+                      ])
+
+    ld = LaunchDescription()
+    ld.add_action(avoidobstacles_cmd)
+
+    return ld
